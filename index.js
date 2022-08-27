6903eec35c71693913a5895f8576d9de0a44fefe
Abhinav Kumar <abhin1509@gmail.com>
2022-08-27 19:15:07 +0530

    upload multiple images to cloud

--- index.js
+++ index.js
@@ -33,11 +33,29 @@ app.post("/mypost", async (req, res) => {
   console.log(req.body);
   console.log(req.files);
 
-  let file = req.files.samplefile;
+  let result;
+  let imageArray = [];
 
-  result = await cloudinary.uploader.upload(file.tempFilePath, {
-    folder: "users",
-  });
+  // For multiple images
+
+  if(req.files) {
+    for (let index = 0; index < req.files.samplefile.length; index++) {
+      result = await cloudinary.uploader.upload(req.files.samplefile[index].tempFilePath, {
+        folder: "users"
+      })
+
+      imageArray.push({
+        public_id: result.public_id,
+        secure_url: result.secure_url
+      })
+    }
+  }
+
+  // For single image
+  // let file = req.files.samplefile;
+  // result = await cloudinary.uploader.upload(file.tempFilePath, {
+  //   folder: "users",
+  // });
 
   console.log(result);
 
@@ -45,7 +63,10 @@ app.post("/mypost", async (req, res) => {
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     result,
+    imageArray
   };
+
+  console.log(details);
   res.send(details);
 });
 
