095633a572229e159fa7529ec6ab753e9e46e15a
Abhinav Kumar <abhin1509@gmail.com>
2022-08-27 17:26:07 +0530

    upload single file to cloudinary

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,15 +17,36 @@ app.set("view engine", "ejs");
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 app.get("/myget", (req, res) => {
   console.log(req.body);
   res.send(req.body);
 });
 
-app.post("/mypost", (req, res) => {
+app.post("/mypost", async (req, res) => {
   console.log(req.body);
-  res.send(req.body);
+  console.log(req.files);
+
+  let file = req.files.samplefile;
+
+  result = await cloudinary.uploader.upload(file.tempFilePath, {
+    folder: "users",
+  });
+
+  console.log(result);
+
+  details = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    result,
+  };
+  res.send(details);
 });
 
 app.get("/mygetform", (req, res) => {
